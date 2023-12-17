b5054fb2413faebc58cf21e705c27fa6a6f0e5af
Can <71851732+Y-Can@users.noreply.github.com>
2023-12-17 22:36:11 +0100

    fix

---
+++ src/app/detail-film/[id].js
@@ -0,0 +1,53 @@
+/* @jsxImportSource client */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../../styles/filmdetail.module.css";
+import { useRouter } from "next/router";
+const FilmDetail = () => {
+	const [film, setFilm] = useState(null);
+	const router = useRouter();Z
+	const { id } = router.query;
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				let apiUrl = "./../api/films/";
+				apiUrl += `?id=${encodeURIComponent(id)}`;
+				const response = await axios.get(apiUrl);
+				const filmData = response.data;
+				setFilm(filmData.films[0]);
+			} catch (error) {
+				console.error("Erreur lors de la requête API", error);
+			}
+		};
+		fetchData();
+	}, [id]);
+	return (
+		<div className={styles.container_col}>
+			{" "}
+			{film ? (
+				<>
+					{" "}
+					<div className={styles.block}>
+						{" "}
+						<div className={styles.titre}>
+							{" "}
+							<h1 className={styles.title}>{film.titre}</h1>{" "}
+						</div>{" "}
+						<div className={styles.containermax}>
+							{" "}
+							<div className={styles.containerRow}>
+								{" "}
+								<img src={film.image} className={styles.img} alt="" />{" "}
+								<p className={styles.p}>{film.description}</p>{" "}
+							</div>{" "}
+						</div>{" "}
+					</div>{" "}
+				</>
+			) : (
+				<p>Chargement en cours...</p>
+			)}{" "}
+			<div className="container-col">{/* Autres éléments de la page */}</div>{" "}
+		</div>
+	);
+};
+export default FilmDetail;
