a445894e01ae259c28b63a5b66f8341a835bf4c9
Can <71851732+Y-Can@users.noreply.github.com>
2023-12-18 00:15:56 +0100

    fixite

--- src/app/detail-film/[id].js
+++ src/app/detail-film/[id].js
@@ -1,17 +1,15 @@
-/* @jsxImportSource client */
-import React, { useEffect, useState } from "react";
+/* @jsxImportSource client */ import React, {
+	useEffect,
+	useState,
+} from "react";
 import axios from "axios";
 import styles from "../../styles/filmdetail.module.css";
-import { useRouter } from "next/router";
-const FilmDetail = () => {
+const FilmDetail = ({ id }) => {
 	const [film, setFilm] = useState(null);
-	const router = useRouter();Z
-	const { id } = router.query;
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				let apiUrl = "./../api/films/";
-				apiUrl += `?id=${encodeURIComponent(id)}`;
+				let apiUrl = `./../api/films/?id=${encodeURIComponent(id)}`;
 				const response = await axios.get(apiUrl);
 				const filmData = response.data;
 				setFilm(filmData.films[0]);
@@ -19,7 +17,9 @@ const FilmDetail = () => {
 				console.error("Erreur lors de la requête API", error);
 			}
 		};
-		fetchData();
+		if (id) {
+			fetchData();
+		}
 	}, [id]);
 	return (
 		<div className={styles.container_col}>
@@ -49,5 +49,9 @@ const FilmDetail = () => {
 			<div className="container-col">{/* Autres éléments de la page */}</div>{" "}
 		</div>
 	);
+};
+FilmDetail.getInitialProps = async (context) => {
+	const { query } = context;
+	return { id: query.id || "" };
 };
 export default FilmDetail;
