50c2e88f56796c7d92827a2793a42c0cabb207bb
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-07 00:46:48 +0100

    fixit

--- src/pages/api/add_film/index.js
+++ src/pages/api/add_film/index.js
@@ -2,7 +2,7 @@ app.post('/api/add_film', async (req, res) => {
     const { image, description, titre } = req.body;
     try {
       const result = await pool.query(
-        'INSERT INTO films (image, description, titre) VALUES ($1, $2, $3) RETURNING id',
+        'INSERT INTO films (image, description, titre) VALUES ($1, $2, $3) RETURNING image, description, titre',
         [image, description, titre]
       );
       const newFilmId = result.rows[0].id;
