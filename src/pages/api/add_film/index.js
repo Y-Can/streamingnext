9ab1a3b40c442bd9497bea5e183cad09650662db
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-07 01:32:18 +0100

    fxi

--- src/pages/api/add_film/index.js
+++ src/pages/api/add_film/index.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-    const { image, description, titre, user_id } = req.body;
+    const { image, description, titre, user } = req.body;
     try {
       const result = await pool.query(
-        'INSERT INTO films (image, description, titre, user_id) VALUES ($1, $2, $3, $4) RETURNING image, description, titre,user_id',
-        [image, description, titre, user_id]
+        'INSERT INTO films (image, description, titre, user) VALUES ($1, $2, $3, $4) RETURNING image, description, titre,user',
+        [image, description, titre, user]
       );
       const newFilmId = result.rows[0];
       res.status(201).json({   message: 'Film ajouté avec succès' ,newFilmId });
