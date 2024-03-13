8fb09c181b2b859c5cc907cc4d6154b00377c96f
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-13 01:05:35 +0100

    add jest

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,50 @@
 import React, { useState } from "react";
 import "../app/globals.css";
 import Link from "next/link";
+
 const Card = ({ films, onMouseEnter = null, onMouseLeave = null }) => {
-	const [hoveredItemId, setHoveredItemId] = useState(null);
-	const handleMouseEnter = (filmId) => {
-		setHoveredItemId(filmId);
-		if (onMouseEnter) {
-			onMouseEnter();
-		}
-	};
-	const handleMouseLeave = () => {
-		setHoveredItemId(null);
-		if (onMouseLeave) {
-			onMouseLeave();
-		}
-	};
-	return (
-		<div className="containerRow">
-			{" "}
-			<div className={"containerCol maxheight"}>
-				{" "}
-				<ul className={"containerRow"}>
-					{" "}
-					{films.map((film) => (
-						<Link  href={{     pathname: '/detail-film/' + film.id,  query: { id: film.id } }}>
-						<li
-							key={film.id}
-							onMouseEnter={() => handleMouseEnter(film.id)}
-							onMouseLeave={handleMouseLeave}
-							className={`li ${hoveredItemId === film.id ? "lihovered" : ""}`}
-							id={`li-${film.id}`}
-						>
-							{" "}
-							<img src={film.image} alt={film.titre} /> <h3>{film.titre}</h3>{" "}
-							<p key={film.id}
+  const [hoveredItemId, setHoveredItemId] = useState(null);
+
+  const handleMouseEnter = (filmId) => {
+    setHoveredItemId(filmId);
+    if (onMouseEnter) {
+      onMouseEnter();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItemId(null);
+    if (onMouseLeave) {
+      onMouseLeave();
+    }
+  };
+
+  return (
+    <div className="containerRow">
+      <div className="containerCol maxheight">
+        <ul className="containerRow">
+          {films.map((film) => (
+            <li
+              key={film.id}
+              onMouseEnter={() => handleMouseEnter(film.id)}
+              onMouseLeave={handleMouseLeave}
+              className={`li ${hoveredItemId === film.id ? "lihovered" : ""}`}
               id={`li-${film.id}`}
-               className={`p ${hoveredItemId === film.id ? "phover" : ""}`}
-              >{film.description}</p>{" "}
-						</li>
-						</Link>
-					))}{" "}
-				</ul>{" "}
-			</div>{" "}
-		</div>
-	);
+            >
+              <Link href={`/detail-film/${film.id}`}>
+                {/* Assurez-vous qu'il n'y a qu'un seul élément <a> ici, et pas d'autres éléments <a> imbriqués */}
+                  <img src={film.image} alt={film.titre} />
+                  <h3>{film.titre}</h3>
+                  <p className={`p ${hoveredItemId === film.id ? "phover" : ""}`}>
+                    {film.description}
+                  </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
+
 export default Card;
