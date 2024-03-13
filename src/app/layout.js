c874eeda0cb0b8a5623dd5d6e5f4679b3329b455
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-13 21:10:08 +0100

    fix client

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,9 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "./../app/globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
