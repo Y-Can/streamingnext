f408338b13607fccabf88698b744dac948bf1258
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-13 21:07:30 +0100

    fix client

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,44 @@ import { useSearchParams } from "next/navigation";
 import axios from "axios";
 import Card from "./../components/Card";
 import "./../app/globals.css";
+import Navbar from "@/components/Navbar";
 const Home = () => {
-
+	const searchParams = useSearchParams();
+	const id = searchParams.get("id");
+	const search = searchParams.get("search");
+	const [films, setFilms] = useState([]);
+	const [searchTerm, setSearchTerm] = useState(search);
+	const [user, setUser] = useState(null); 
+		
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				let apiUrl = "./api/films";
+				if (id !== null) {
+					apiUrl += `?id=${encodeURIComponent(id)}`;
+				} else if (searchTerm !== null) {
+					apiUrl += `?search=${encodeURIComponent(searchTerm)}`;
+				}
+				const res = await axios.get(apiUrl);
+				const data = res.data;
+				setFilms(data.films || []);
+			} catch (error) {
+				console.error("Erreur lors de la récupération des films", error);
+				setFilms([]);
+			}
+		};
+		fetchData();
+	}, [id, searchTerm]);
+	const handleSearchTermChange = (newSearchTerm) => {
+		setSearchTerm(newSearchTerm);
+	};
 	return (
 		<div className="containerCol">
 			
-			{/* <Navbar
+			<Navbar
 				searchTerm={searchTerm}
 				onSearchTermChange={handleSearchTermChange}
-			/> */}
+			/>
 			<div className="rowCenter">
 				
 				<h1>Liste des Films</h1>
