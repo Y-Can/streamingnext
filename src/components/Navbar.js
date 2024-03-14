fdc8de049eb58c0c23ac1c73d3ad63465a51589b
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-14 22:22:20 +0100

    fix searhc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     const id = searchParams.get("id");
 	const searchTerm = searchParams.get("search");
     const router = useRouter();
-    // const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("");
     const [user, setUser] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
 
