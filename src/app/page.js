7d29798b99d66d29b3ec46d06bc328e48aabe458
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-15 00:26:31 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,78 @@
-"use client";
+// pages/index.js
+"use client"
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import axios from "axios";
-import Card from "../components/Card";
+import Card from "./../components/Card";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
 const Home = () => {
-  const router = useRouter();
-  const [films, setFilms] = useState([]);
+	const router = useRouter();
 
-  // Utilisation de useEffect pour écouter les changements de l'URL
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let apiUrl = "/api/films";
-        const { id, search } = router.query; // Récupération des paramètres de l'URL
+	const searchParams = useSearchParams();
+	const id = searchParams.get("id");
+	const search = searchParams.get("search");
+	const [films, setFilms] = useState([]);
+	const [searchTerm, setSearchTerm] = useState(search);
+	const [user, setUser] = useState(null); 
+	
+		
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			try {
+				let apiUrl = "./api/films";
+				if (id !== null) {
+					apiUrl += `?id=${encodeURIComponent(id)}`;
+				} else if (searchTerm !== null) {
+					apiUrl += `?search=${encodeURIComponent(searchTerm)}`;
+				}
+				const res = await axios.get(apiUrl);
+				const data = res.data;
+				setFilms(data.films || []);
+			} catch (error) {
+				console.error("Erreur lors de la récupération des films", error);
+				setFilms([]);
+			}
+			
+		};
+			// test
+			const fetchFilm = async () => {
+				try {
+					let apiUrl = "/../api/films";
+					if (id !== null) {
+						apiUrl += `?id=${encodeURIComponent(id)}`;
+					} else if (searchTerm !== null) {
+						apiUrl += `?search=${encodeURIComponent(searchTerm)}`;
+					}
+					const res = await axios.get(apiUrl);
+					const data = res.data;
+					setFilms(data.films || []);
+				} catch (error) {
+					console.error("Erreur lors de la récupération des films", error);
+					setFilms([]);
+				}
+				
+			};
+		fetchFilm()
+		fetchData();
+		router.replace(router.asPath)
+	}, [id, searchTerm]);
+	const handleSearchTermChange = (newSearchTerm) => {
+		setSearchTerm(newSearchTerm);
+	};
+	return (
 
-        if (id) {
-          apiUrl += `?id=${encodeURIComponent(id)}`;
-        } else if (search) {
-          apiUrl += `?search=${encodeURIComponent(search)}`;
-        }
+			<div className="containerCol">
+				<div className="rowCenter">
+					
+					<h1>Liste des Films</h1>
+				</div>
+				<div className="containerRow">
+					<Card films={films} />
+				</div>
+			</div>
 
-        const res = await axios.get(apiUrl);
-        const data = res.data;
-        setFilms(data.films || []);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des films", error);
-        setFilms([]);
-      }
-    };
-
-    if (router.isReady) {
-      fetchData();
-    }
-  }, [router.isReady, router.query]); // Dépendances mises à jour pour inclure router.isReady et router.query
-
-  return (
-    <div className="containerCol">
-      <Navbar onSearch={handleSearchTermChange} /> {/* Assurez-vous que Navbar accepte une prop onSearch et la gère correctement */}
-      <div className="rowCenter">
-        <h1>Liste des Films</h1>
-      </div>
-      <div className="containerRow">
-        <Card films={films} />
-      </div>
-    </div>
-  );
+	);
 };
-
 export default Home;
