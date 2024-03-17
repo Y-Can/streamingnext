18f2364967b55e4ff88c13223254f10ae5052d36
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-17 06:05:34 +0100

    sc

--- src/app/add_film/page.js
+++ src/app/add_film/page.js
@@ -1,6 +1,8 @@
-"use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// Assurez-vous d'importer votre fichier CSS ici pour qu'il soit pris en compte
+import './LoginSignupForm.css';
+
 const NouvellePage = () => {
 	const [user, setUser] = useState(null);
 	useEffect(() => {
@@ -17,65 +19,65 @@ const NouvellePage = () => {
 		};
 		fetchData();
 	}, []);
+
 	const [titre, setTitre] = useState("");
 	const [image, setImage] = useState("");
 	const [description, setDescription] = useState("");
-	const handleImageChange = (event) => {
-		const selectedImage = event.target.files[0];
-		setImage(selectedImage);
-	};
+
 	const handleFormSubmit = async (event) => {
 		event.preventDefault();
 
-			const response = await axios.post("../../../api/add_film", {user:user.id,titre:titre,image:image,description:description});
-			if (response.status === 201) {
-				console.log("Film ajouté :", response.data);
-			} else {
-				console.error("Erreur lors de l'ajout du film");
-			}
+		const formData = new FormData();
+		formData.append('titre', titre);
+		formData.append('image', image);
+		formData.append('description', description);
+
+		const response = await axios.post("../../../api/add_film", formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data',
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+		});
+
+		if (response.status === 201) {
+			console.log("Film ajouté :", response.data);
+		} else {
+			console.error("Erreur lors de l'ajout du film");
+		}
 	};
+
 	return (
-		<div className="container-row">
-			
-			<div className="container-col">
-				
+		<div className="container">
+			<div className="signup-container">
 				<h1>Ajout de film</h1>
 				<form onSubmit={handleFormSubmit} encType="multipart/form-data">
-					
-					<div className="container-col">
-						
-						<label>
-							
-							Titre
-							<input
-								type="text"
-								value={titre}
-								onChange={(e) => setTitre(e.target.value)}
-							/>
-						</label>
-						<label>
-							
-							Image
-							<input
-								value={image}
-								type="text"
-								onChange={(e) => setImage(e.target.value)}
-							/>
-						</label>
-						<label>
-							
-							Description
-							<input
-								type="text"
-								value={description}
-								onChange={(e) => setDescription(e.target.value)}
-							/>
-						</label>
+					<div className="form">
+						<div className="label">Titre</div>
+						<input
+							className="input"
+							type="text"
+							value={titre}
+							onChange={(e) => setTitre(e.target.value)}
+						/>
+						<div className="label">Image</div>
+						<input
+							className="input"
+							type="file"
+							onChange={handleImageChange}
+						/>
+						<div className="label">Description</div>
+						<input
+							className="input"
+							type="text"
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+						/>
 					</div>
-					<button type="submit">Ajouter</button>
+					<button className="button" type="submit">Ajouter</button>
 				</form>
 			</div>
 		</div>
 	);
 };
+
 export default NouvellePage;
