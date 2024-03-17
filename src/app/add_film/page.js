d3f823c3a72e8df9aca9fc6945a259573b38f921
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-17 06:15:50 +0100

    test

--- src/app/add_film/page.js
+++ src/app/add_film/page.js
@@ -1,84 +1,87 @@
-"use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// Assurez-vous d'importer votre fichier CSS ici pour qu'il soit pris en compte
-import './LoginSignupForm.css';
+import styles from "../styles/addfilm.module.css"; 
 
 const NouvellePage = () => {
-	const [user, setUser] = useState(null);
-	useEffect(() => {
-		const fetchData = async () => {
-			if (typeof window !== "undefined") {
-				const token = localStorage.getItem("token");
-					const response = await axios.get("../../../api/user", {
-						headers: { Authorization: `Bearer ${token}` },
-					});
-					const { id } = response.data;
-					const userData = { id };
-					setUser(userData);
-			}
-		};
-		fetchData();
-	}, []);
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            if (typeof window !== "undefined") {
+                const token = localStorage.getItem("token");
+                const response = await axios.get("../../../api/user", {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                const { id } = response.data;
+                const userData = { id };
+                setUser(userData);
+            }
+        };
+        fetchData();
+    }, []);
 
-	const [titre, setTitre] = useState("");
-	const [image, setImage] = useState("");
-	const [description, setDescription] = useState("");
+    const [titre, setTitre] = useState("");
+    const [image, setImage] = useState("");
+    const [description, setDescription] = useState("");
 
-	const handleFormSubmit = async (event) => {
-		event.preventDefault();
+    const handleImageChange = (event) => {
+        const selectedImage = event.target.files[0];
+        setImage(selectedImage);
+    };
 
-		const formData = new FormData();
-		formData.append('titre', titre);
-		formData.append('image', image);
-		formData.append('description', description);
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-		const response = await axios.post("../../../api/add_film", formData, {
-			headers: {
-				'Content-Type': 'multipart/form-data',
-				Authorization: `Bearer ${localStorage.getItem("token")}`,
-			},
-		});
+        const formData = new FormData();
+        formData.append('titre', titre);
+        formData.append('image', image);
+        formData.append('description', description);
 
-		if (response.status === 201) {
-			console.log("Film ajouté :", response.data);
-		} else {
-			console.error("Erreur lors de l'ajout du film");
-		}
-	};
+        const response = await axios.post("../../../api/add_film", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
 
-	return (
-		<div className="container">
-			<div className="signup-container">
-				<h1>Ajout de film</h1>
-				<form onSubmit={handleFormSubmit} encType="multipart/form-data">
-					<div className="form">
-						<div className="label">Titre</div>
-						<input
-							className="input"
-							type="text"
-							value={titre}
-							onChange={(e) => setTitre(e.target.value)}
-						/>
-						<div className="label">Image</div>
-						<input
-							className="input"
-							type="file"
-							onChange={handleImageChange}
-						/>
-						<div className="label">Description</div>
-						<input
-							className="input"
-							type="text"
-							value={description}
-							onChange={(e) => setDescription(e.target.value)}
-						/>
-					</div>
-					<button className="button" type="submit">Ajouter</button>
-				</form>
-			</div>
-		</div>
-	);
+        if (response.status === 201) {
+            console.log("Film ajouté :", response.data);
+        } else {
+            console.error("Erreur lors de l'ajout du film");
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.signupContainer}>
+                <h1>Ajout de film</h1>
+                <form onSubmit={handleFormSubmit} encType="multipart/form-data">
+                    <div className={styles.form}>
+                        <div className={styles.label}>Titre</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={titre}
+                            onChange={(e) => setTitre(e.target.value)}
+                        />
+                        <div className={styles.label}>Image</div>
+                        <input
+                            className={styles.input}
+                            type="file"
+                            onChange={handleImageChange}
+                        />
+                        <div className={styles.label}>Description</div>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </div>
+                    <button className={styles.button} type="submit">Ajouter</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default NouvellePage;
