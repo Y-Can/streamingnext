7d9bbd603225cbee656b5e8fc55446948c0ac1ce
Can <71851732+Y-Can@users.noreply.github.com>
2024-03-17 01:46:09 +0100

    sdcs

--- src/app/detail-film/[id]/page.js
+++ src/app/detail-film/[id]/page.js
@@ -45,7 +45,7 @@ const FilmDetail = ({ params }) => {
 							<div className={styles.containerRow}>
 								
 								<img src={film?.image} className={styles.img} alt="" />
-								<a href="https://uqload.to/5gpwdhlx14ja.html" target={_blank}><img src="https://en8.uqload.to/i/01/02383/5gpwdhlx14ja_t.jpg" />@VoirSerieFilm See You Yesterday (2019)</a>[664x384, 01:27:12]
+								<a href="https://uqload.to/5gpwdhlx14ja.html" ><img src="https://en8.uqload.to/i/01/02383/5gpwdhlx14ja_t.jpg" />@VoirSerieFilm See You Yesterday (2019)</a>[664x384, 01:27:12]
 								<div className={styles.containerCol}>
 									
 									{/* <video  className={styles.video} controls>
