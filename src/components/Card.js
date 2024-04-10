3725323d2704136ac7f7c5241d82853b280d085f
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-10 22:29:42 +0200

    test

--- src/components/Card.js
+++ src/components/Card.js
@@ -19,32 +19,31 @@ const Card = ({ films, onMouseEnter = null, onMouseLeave = null }) => {
     }
   };
 
+ 
   return (
-    <div className="containerRowUl">
-      <div className="containerCol maxheight">
-        <ul className="containerRowUl">
-          {films.map((film) => (
-            <li
-              key={film.id}
-              onMouseEnter={() => handleMouseEnter(film.id)}
-              onMouseLeave={handleMouseLeave}
-              className={`card transition`}
-              id={`li-${film.id}`}
-            >
-              <Link href={`/detail-film/${film.id}`}>
-                {/* Assurez-vous qu'il n'y a qu'un seul élément <a> ici, et pas d'autres éléments <a> imbriqués */}
-                  <img src={film.image} alt={film.titre} />
-                  <h3 className={'transition'}>{film.titre}</h3>
-                  <p >
-                    {film.description}
-                  </p>
-                  <div class="cta-container transition"><a href="#" class="cta">Call to action</a></div>
-  <div class="card_circle transition"></div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="container">
+      {films.map((film) => (
+        <div
+          key={film.id}
+          className={`card ${hoveredItemId === film.id ? "hovered" : ""}`}
+          onMouseEnter={() => setHoveredItemId(film.id)}
+          onMouseLeave={() => setHoveredItemId(null)}
+        >
+          <div className="card_circle"></div> {/* Cercle décoratif */}
+          <h2>{film.titre}</h2>
+          <p>{film.description}</p>
+          <div className="cta-container">
+            <Link href={`/detail-film/${film.id}`}>
+              <a className="cta">En savoir plus</a>
+            </Link>
+          </div>
+          <Link href={`/detail-film/${film.id}`}>
+            <a>
+              <img src={film.image} alt={film.titre} />
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
