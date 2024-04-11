82980b7a14603142989228bc01f0070ddaca4fb7
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-11 09:23:34 +0200

    vic

--- src/components/Videoplayer.js
+++ src/components/Videoplayer.js
@@ -34,7 +34,7 @@ return (
             <p className={styles.description}>{description}</p>
           </div>
           <div className={styles.playerContainer}>
-            <video  className={styles.video} controls poster={image} src="./../../public/inter.mp4" />
+            <video ref={videoRef} className={styles.video} controls src="./../../public/inter.mp4" />
 
             <div className={styles.playerControls}>
               <button onClick={togglePlay} className={styles.button + ' ' + (isPlaying ? styles.pause : styles.play)} aria-label={isPlaying ? 'Pause' : 'Play'}></button>
