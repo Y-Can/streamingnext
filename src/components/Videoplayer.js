9c58240128148d5582251b48a66320cbbae92648
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-11 07:55:32 +0200

    fix

--- src/components/Videoplayer.js
+++ src/components/Videoplayer.js
@@ -1,84 +1,57 @@
-import React, { useState, useRef, useEffect } from "react";
+// Footer.js
+import React from "react";
 import "../app/globals.css";
 import styles from '../app/styles/videoplayer.module.css';
+import { Helmet } from 'react-helmet';
+// Créez un fichier Footer.module.css pour les styles
+const VideoPlayer = ({title, videoSrc, description, image}) => {
 
-const VideoPlayer = ({ title, videoSrc, description, image }) => {
-  const videoRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(0.5);
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const updateProgress = () => {
-      const progress = (videoRef.current.currentTime / videoRef.current.duration) * 100;
-      setProgress(progress);
-    };
-
-    const videoElement = videoRef.current;
-    videoElement.addEventListener("timeupdate", updateProgress);
-
-    return () => videoElement.removeEventListener("timeupdate", updateProgress);
-  }, []);
-
-  const togglePlay = () => {
-    if (isPlaying) {
-      videoRef.current.pause();
-    } else {
-      videoRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleProgressClick = (e) => {
-    const newTime = (e.nativeEvent.offsetX / e.target.clientWidth) * videoRef.current.duration;
-    videoRef.current.currentTime = newTime;
-  };
-
-  const handleVolumeChange = (e) => {
-    const newVolume = e.target.value;
-    videoRef.current.volume = newVolume;
-    setVolume(newVolume);
-  };
-
-  const handleSkip = (amount) => {
-    videoRef.current.currentTime += amount;
-  };
-
-  const toggleFullScreen = () => {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
-    } else {
-      videoRef.current.parentNode.requestFullscreen();
-    }
-  };
-
-  return (
+return (
     <main className={styles.wrapper}>
       <div className={styles.player}>
         <div className={styles.playerOverlay} data-fullscreen="false">
           <div className={styles.container}>
             <div className={styles.informationContainer}>
               <h1 className={styles.title}>{title}</h1>
-              <p className={styles.description}>{description}</p>
+              <p className={styles.description}>
+                {description}
+              </p>
             </div>
             <div className={styles.playerContainer}>
-              <video className={styles.video} ref={videoRef} poster={image} src={videoSrc} />
+              <div className={styles.videoProgress}>
+                <div className={styles.videoProgressFilled}></div>
+              </div>
               <div className={styles.playerControls}>
-                <button className={styles.button} onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-                <button className={styles.button} onClick={() => handleSkip(-10)}>Rewind 10s</button>
-                <button className={styles.button} onClick={() => handleSkip(10)}>Forward 10s</button>
-                <input className={styles.volumeProgress} type="range" min="0" max="1" step="0.05" value={volume} onChange={handleVolumeChange} />
-                <button className={styles.button} onClick={toggleFullScreen}>Full Screen</button>
-                <div className={styles.videoProgress} onClick={handleProgressClick}>
-                  <div className={styles.videoProgressFilled} style={{ width: `${progress}%` }}></div>
+                <div className={styles.playerButtons}>
+                  <button aria-label="play" className={styles.button + ' ' + styles.play} title="play" type="button"></button>
+                  <button aria-label="pause" className={styles.button + ' ' + styles.pause} hidden title="pause" type="button"></button>
+                  <button aria-label="backward" className={styles.button + ' ' + styles.backward} title="backward" type="button"></button>
+                  <button aria-label="forward" className={styles.button + ' ' + styles.forward} title="forward" type="button"></button>
+                  <button aria-label="volume" className={styles.button + ' ' + styles.volume} title="volume" type="button"></button>
+                  <button aria-label="silence" className={styles.button + ' ' + styles.silence} hidden title="silence" type="button"></button>
+                  <div className={styles.volumeProgress}>
+                    <div className={styles.volumeProgressFilled}></div>
+                  </div>
+                  <div className={styles.timeContainer}>
+                    <p className={styles.currentTime}>0:00</p>
+                    <p className={styles.timeSeparator}>/</p>
+                    <p className={styles.durationVideo}>0:00</p>
+                  </div>
+                </div>
+                <div className={styles.expandContainer}>
+                  <button aria-label="expand" className={styles.button + ' ' + styles.expand} title="expand" type="button"></button>
+                  <button aria-label="reduce" className={styles.button + ' ' + styles.reduce} hidden title="reduce" type="button"></button>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <video className={styles.video} poster={image} src={videoSrc}></video>
       </div>
+      <Helmet>
+        <script src="./js/index.js" crossorigin="anonymous" async></script>
+      </Helmet>
     </main>
   );
 };
-
 export default VideoPlayer;
