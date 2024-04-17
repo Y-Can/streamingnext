58862322f78013289a0c9b45ff31ac2a165e9b52
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-17 04:44:35 +0200

    fd

--- src/pages/api/add_serie/index.js
+++ src/pages/api/add_serie/index.js
@@ -1,22 +1,28 @@
 import pool from "../db";
 
 export default async function handler(req, res) {
-    const { image, description, titre, user } = req.body;
+    const { image, description, titre, user, url, poster, ba } = req.body;
+   // Validation des données requises
+   console.log(req.body);
+
+   if (!titre) {
+    return res.status(400).json({ error: 'Le titre est requis et ne peut pas être vide.' });
+}
     try {
-        // Assurez-vous que les types de données sont corrects et que la table et les colonnes existent
         const result = await pool.query(
-            'INSERT INTO series (image, description, titre, user_id) VALUES ($1, $2, $3, $4) RETURNING image, description, titre, user_id',
-            [image, description, titre, user]
+            'INSERT INTO series (titre, description, image, user_id, url, poster, ba) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+            [titre, description, image, user, url, poster, ba]
         );
-        const newSerie = result.rows[0]; // Obtient le premier enregistrement de résultat, qui est la nouvelle série
+        const newSerie = result.rows[0];
         res.status(201).json({
             message: 'Série ajoutée avec succès',
-            serie: newSerie // Renvoie l'objet de la série incluant l'ID
+            serie: newSerie
         });
     } catch (error) {
-        console.error('Erreur lors de l\'ajout de la série', error);
+        console.error('Erreur lors de l\'ajout de la série', error,req);
         res.status(500).json({
-            error: 'Erreur interne du serveur'
+            error: 'Erreur interne du serveur',
+            detail: error.message // Ajout de cette ligne pour plus de détails sur l'erreur
         });
     }
 };
