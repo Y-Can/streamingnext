66d9a9955c4dab3b2652dfd175318ee4e7a096e8
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-17 03:45:03 +0200

    testfix

--- src/pages/api/series/index.js
+++ src/pages/api/series/index.js
@@ -44,23 +44,21 @@ export default async function handler(req, res) {
         }
     } else if (req.method === "POST") {
         try {
-            console.log('la req body',req.body);
-            const { user } = req.body;
-            const userId = user
-            const puser = await pool.query("SELECT type FROM users where id = $1", [
-                userId,
-            ]);
-            if (puser.type === "ADMIN") {
-                const { titre, description, image } = req.body;
+            // const userId = user
+            // const puser = await pool.query("SELECT type FROM users where id = $1", [
+            //     userId,
+            // ]);
+            // if (puser.type === "ADMIN") {
+                const { titre, description, image, user } = req.body;
                 const result = await pool.query(
                     "INSERT INTO series (titre, description, image, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
-                    [titre, description, image, userId] // Make sure to include userId in the values array
+                    [titre, description, image, user] // Make sure to include userId in the values array
                 );
                 const serieAjoutee = result.rows[0];
                 res.status(201).json(serieAjoutee);
-            } else {
-                res.status(500).json({ error: "Erreur lors de l'envoi de la série" });
-            }
+            // } else {
+            //     res.status(500).json({ error: "Erreur lors de l'envoi de la série" });
+            // }
         } catch (error) {
             console.error("Erreur lors de l'envoi de la série", error);
             res.status(500).json({ error: "Erreur lors de l'envoi de la série" });
