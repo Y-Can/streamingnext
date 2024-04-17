7184d92e74d4071964b17a28d96905f3be5f0d22
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-17 04:24:33 +0200

    fxf

--- src/pages/api/add_serie/index.js
+++ src/pages/api/add_serie/index.js
@@ -1,12 +1,12 @@
 import pool from "../db";
 
 export default async function handler(req, res) {
-    const { image, description, titre, user_id, url, poster, ba } = req.body;
+    const { image, description, titre, user, url, poster, ba } = req.body;
 
     try {
         const result = await pool.query(
             'INSERT INTO series (titre, description, image, user_id, url, poster, ba) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-            [titre, description, image, user_id, url, poster, ba]
+            [titre, description, image, user, url, poster, ba]
         );
         const newSerie = result.rows[0];
         res.status(201).json({
