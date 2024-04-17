d463fa80367467abc1d4a6448fda19800f85c5eb
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-17 04:21:46 +0200

    fuc

--- src/pages/api/add_serie/index.js
+++ src/pages/api/add_serie/index.js
@@ -2,20 +2,22 @@ import pool from "../db";
 
 export default async function handler(req, res) {
     const { image, description, titre, user_id, url, poster, ba } = req.body;
+
     try {
         const result = await pool.query(
             'INSERT INTO series (titre, description, image, user_id, url, poster, ba) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
             [titre, description, image, user_id, url, poster, ba]
         );
-        const newSerie = result.rows[0]; // Obtient le premier enregistrement de résultat, qui est la nouvelle série
+        const newSerie = result.rows[0];
         res.status(201).json({
             message: 'Série ajoutée avec succès',
-            serie: newSerie // Renvoie l'objet de la série incluant l'ID et les autres champs
+            serie: newSerie
         });
     } catch (error) {
         console.error('Erreur lors de l\'ajout de la série', error);
         res.status(500).json({
-            error: 'Erreur interne du serveur'
+            error: 'Erreur interne du serveur',
+            detail: error.message // Ajout de cette ligne pour plus de détails sur l'erreur
         });
     }
 };
