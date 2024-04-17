815448256b929c4623404983c35c551766823f2d
Can <71851732+Y-Can@users.noreply.github.com>
2024-04-17 04:19:17 +0200

    fcx

--- src/pages/api/add_serie/index.js
+++ src/pages/api/add_serie/index.js
@@ -1,17 +1,16 @@
 import pool from "../db";
 
 export default async function handler(req, res) {
-    const { image, description, titre, user } = req.body;
+    const { image, description, titre, user_id, url, poster, ba } = req.body;
     try {
-        // Assurez-vous que les types de données sont corrects et que la table et les colonnes existent
         const result = await pool.query(
-            'INSERT INTO series (image, description, titre, user_id) VALUES ($1, $2, $3, $4) RETURNING image, description, titre, user_id',
-            [image, description, titre, user]
+            'INSERT INTO series (titre, description, image, user_id, url, poster, ba) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+            [titre, description, image, user_id, url, poster, ba]
         );
         const newSerie = result.rows[0]; // Obtient le premier enregistrement de résultat, qui est la nouvelle série
         res.status(201).json({
             message: 'Série ajoutée avec succès',
-            serie: newSerie // Renvoie l'objet de la série incluant l'ID
+            serie: newSerie // Renvoie l'objet de la série incluant l'ID et les autres champs
         });
     } catch (error) {
         console.error('Erreur lors de l\'ajout de la série', error);
