5166cf467321a138600cd7b94ac32be8da4e7e42
Can <71851732+Y-Can@users.noreply.github.com>
2024-05-02 19:18:01 +0200

    test

--- src/app/add_film/page.js
+++ src/app/add_film/page.js
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/addfilm.module.css"; 
@@ -23,31 +22,60 @@ const NouvellePage = () => {
     const [titre, setTitre] = useState("");
     const [image, setImage] = useState("");
     const [description, setDescription] = useState("");
+    const [video, setVideo] = useState(null); // Ajout de l'état pour la vidéo
 
     const handleImageChange = (event) => {
         const selectedImage = event.target.files[0];
         setImage(selectedImage);
     };
 
+    const handleVideoChange = (event) => {
+        const selectedVideo = event.target.files[0];
+        setVideo(selectedVideo); // Gérer le changement de la vidéo
+    };
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
-        const formData = new FormData();
-        formData.append('titre', titre);
-        formData.append('image', image);
-        formData.append('description', description);
+        // Créer FormData pour Vimeo
+        const videoFormData = new FormData();
+        videoFormData.append('file_data', video);
+        videoFormData.append('name', titre);
+        videoFormData.append('description', description);
 
-        const response = await axios.post("../../../api/add_film", formData, {
+        // Envoyer la vidéo à Vimeo et obtenir l'URL
+        const vimeoResponse = await axios.post("https://api.vimeo.com/me/videos", videoFormData, {
             headers: {
-                'Content-Type': 'multipart/form-data',
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
+                'Authorization': `8835816b62f0f4fc06a438d43407348d`,
+                'Content-Type': 'multipart/form-data'
+            }
         });
 
-        if (response.status === 201) {
-            console.log("Film ajouté :", response.data);
+        if (vimeoResponse.status === 200) {
+            const videoUrl = vimeoResponse.data.link;
+
+            // Créer FormData pour l'API interne
+            const formData = new FormData();
+            formData.append('titre', titre);
+            formData.append('image', image);
+            formData.append('description', description);
+            formData.append('videoUrl', videoUrl); // Ajouter l'URL de la vidéo à formData
+
+            // Envoyer formData à l'API interne
+            const response = await axios.post("../../../api/add_film", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+
+            if (response.status === 201) {
+                console.log("Film ajouté avec vidéo :", response.data);
+            } else {
+                console.error("Erreur lors de l'ajout du film");
+            }
         } else {
-            console.error("Erreur lors de l'ajout du film");
+            console.error("Erreur lors du téléchargement de la vidéo sur Vimeo");
         }
     };
 
@@ -69,6 +97,12 @@ const NouvellePage = () => {
                             className={styles.input}
                             type="file"
                             onChange={handleImageChange}
+                        />
+                        <div className={styles.label}>Vidéo</div>
+                        <input
+                            className={styles.input}
+                            type="file"
+                            onChange={handleVideoChange}
                         />
                         <div className={styles.label}>Description</div>
                         <input
